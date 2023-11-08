b0068a7c56f0dee83eafabb312e23520990a236b
Artem Korolov <adkorolov@gmail.com>
2023-11-08 11:12:41 +0200

    add questions.js

---
+++ questions.js
@@ -0,0 +1,52 @@
+const cuestions = [
+    {
+        question: "Question 1",
+        answer: "Answer 1",
+        options: [
+            "Option 1",
+            "Option 2",
+            "Option 3",
+            "Option 4",
+        ]    
+    },
+    {
+        question: "Question 2",
+        answer: "Answer 2",
+        options: [
+            "Option 1",
+            "Option 2",
+            "Option 3",
+            "Option 4",
+        ]    
+    },
+    {
+        question: "Question 3",
+        answer: "Answer 3",
+        options: [
+            "Option 1",
+            "Option 2",
+            "Option 3",
+            "Option 4",
+        ]    
+    },
+    {
+        question: "Question 4",
+        answer: "Answer 4",
+        options: [
+            "Option 1",
+            "Option 2",
+            "Option 3",
+            "Option 4",
+        ]    
+    },
+    {
+        question: "Question 5",
+        answer: "Answer 5",
+        options: [
+            "Option 1",
+            "Option 2",
+            "Option 3",
+            "Option 4",
+        ]    
+    }
+]
