998f29655e63cf949d52fe605bb5d887973b105b
Artem Korolov <adkorolov@gmail.com>
2023-11-08 13:07:04 +0200

    add main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+
+const quiz = document.querySelector('.quiz');
+const warning = document.querySelector('.quiz');
+const btnNext = document.querySelector('.quiz-btn-next');
+
+let count = 0;
+let userScore = 0;
